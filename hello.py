5cc0cf66e15856405c49d66c5306159483dd6f93
Abdul Rahman Ansari <ansari.abr@gmail.com>
2016-09-05 13:52:06 +0530

    inital commit

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 import os
+import json
+import urllib2
+from bs4 import BeautifulSoup
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "My First Python Application!"
+    return "My First Python Scraper!"
+
+@app.route("/offer")
+def getOffers():
+    return "Offer List"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
